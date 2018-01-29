c82bc734d58364aca673a28003e94d6ef6272ace
Brad Osgood <bosgood@gmail.com>
2018-01-28 20:31:09 -0500

    Reorganize into lib

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+extern crate clap;
+extern crate colored;
+extern crate serde_json;
+
+use colored::*;
+use serde_json::{Error, Value};
+
+pub fn reformat_str(input: &str) -> Result<String, Error> {
+    let val: Value = serde_json::from_str(input)?;
+    return reformat_value(val);
+}
+
+fn reformat_value(val: Value) -> Result<String, Error> {
+    let out = match val {
+        Value::Number(l) => format!("{}", l), //.green(),
+        Value::Bool(l) => format!("{}", l),   //.green(),
+        Value::Null => String::from("null"),  //.green(),
+        Value::String(l) => format!("{}", l), //.green(),
+        Value::Array(arr) => {
+            let mut buf = String::new();
+
+            buf.push('[');
+            for (i, item) in arr.iter().enumerate() {
+                if i > 0 {
+                    buf.push(' ');
+                }
+                buf.push_str(&reformat_value(item.clone())?);
+            }
+            buf.push(']');
+            buf
+            // colored::ColoredString::from(buf.as_str())
+        }
+        Value::Object(obj) => {
+            let mut buf = String::new();
+            for k in obj.keys() {
+                let val = obj.get(k);
+                match val {
+                    Some(v) => {
+                        let formatted = reformat_value(v.clone())?;
+                        buf.push_str(&format!("{k}={v} ", k = k, v = formatted));
+                    }
+                    None => {}
+                }
+            }
+            buf
+        }
+    };
+
+    Ok(out)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn reformat_obj() {
+        let a = super::reformat_str("{\"a\": 17}").unwrap();
+        assert_eq!(a, "a=17 ");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,11 @@ extern crate clap;
 extern crate colored;
 extern crate serde_json;
 
+mod lib;
+
 use std::process;
 use std::io;
-use colored::*;
-use serde_json::{Error, Value};
 use clap::App;
-
-fn reformat_str(input: &str) -> Result<String, Error> {
-    let val: Value = serde_json::from_str(input)?;
-    return reformat_value(val);
-}
-
-fn reformat_value(val: Value) -> Result<String, Error> {
-    let out = match val {
-        Value::Number(l) => format!("{}", l), //.green(),
-        Value::Bool(l) => format!("{}", l),   //.green(),
-        Value::Null => String::from("null"),  //.green(),
-        Value::String(l) => format!("{}", l), //.green(),
-        Value::Array(arr) => {
-            let mut buf = String::new();
-
-            buf.push('[');
-            for (i, item) in arr.iter().enumerate() {
-                if i > 0 {
-                    buf.push(' ');
-                }
-                buf.push_str(&reformat_value(item.clone())?);
-            }
-            buf.push(']');
-            buf
-            // colored::ColoredString::from(buf.as_str())
-        }
-        Value::Object(obj) => {
-            let mut buf = String::new();
-            for k in obj.keys() {
-                let val = obj.get(k);
-                match val {
-                    Some(v) => {
-                        let formatted = reformat_value(v.clone())?;
-                        buf.push_str(&format!("{k}={v} ", k = k, v = formatted));
-                    }
-                    None => {}
-                }
-            }
-            buf
-        }
-    };
-
-    Ok(out)
-}
 
 fn main() {
     App::new("structy")
@@ -65,7 +21,7 @@ fn main() {
                 if n == 0 {
                     process::exit(0);
                 }
-                match reformat_str(&line) {
+                match lib::reformat_str(&line) {
                     Ok(l) => println!("{}", l),
                     Err(error) => println!("parsing error: {}", error),
                 }
@@ -77,14 +33,5 @@ fn main() {
                 process::exit(1)
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn reformat_obj() {
-        let a = super::reformat_str("{\"a\": 17}").unwrap();
-        assert_eq!(a, "a=17 ");
     }
 }
